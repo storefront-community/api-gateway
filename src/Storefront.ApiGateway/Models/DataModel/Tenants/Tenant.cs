97aaf0b62255cdd48ce6a6e9792a3b4e4d07d05d
Marx JMoura <marx.jmoura@gmail.com>
2019-07-25 21:02:19 -0300

    Initialize Tenant.CreatedAt

--- src/Storefront.ApiGateway/Models/DataModel/Tenants/Tenant.cs
+++ src/Storefront.ApiGateway/Models/DataModel/Tenants/Tenant.cs
@@ -7,7 +7,7 @@ namespace Storefront.ApiGateway.Models.DataModel.Tenants
     public class Tenant
     {
         public long Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string BusinessName { get; set; }
         public string BillingEmail { get; set; }
         public ICollection<ApplicationRole> Roles { get; set; }
